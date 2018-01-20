9a1af13f4a061d2df6ce0b7beb54f2544c8f7508
ikbale <ikbale94@gmail.com>
2018-01-20 15:40:58 +0100

    update database.php.

--- homestead.app/config/database.php
+++ homestead.app/config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '159.203.173.235'),
+            'port' => env('DB_PORT', '22'),
+            'database' => env('DB_DATABASE', 'flightdb'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'nodejs'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
